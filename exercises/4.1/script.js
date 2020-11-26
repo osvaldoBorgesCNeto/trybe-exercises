a2507cdf991bf0a1d4dbbe0d6856e4cae3f8d279
Osvaldo Borges <netoo70@gmail.com>
2020-11-26 14:05:30 -0300

    Exercicios concluidos

--- exercises/4.1/script.js
+++ exercises/4.1/script.js
@@ -148,8 +148,12 @@ let venda = 300;
 let lucro = venda - custoTotal;
 let lucroF = lucro * 1000;
 
-console.log(lucro);
-console.log(lucroF);
+if (custo > 0){
+    console.log(lucro);
+    console.log(lucroF);
+}else {
+    console.log('Custo invalido')
+}
 
 // 11
 console.log('Exercicio 11');
@@ -158,14 +162,19 @@ let salarioINSS = null;
 let salarioIR = null;
 let salarioLiquido = null;
 
-if (salarioBruto < 1556.94) {
-    salarioINSS = salarioBruto * 0.92;
-}else if (salarioBruto > 1556.95 && salarioBruto < 2594.92) {
-    salarioINSS = salarioBruto * 0.91;
-}else if (salarioBruto > 2594.93 && salarioBruto < 5189.82) {
-    salarioINSS = salarioBruto * 0.89;
-}else if (salarioBruto > 5189.82) {
-    salarioINSS = salarioBruto - 570.88;
+if (salarioBruto >= 0) {
+    if (salarioBruto < 1556.94) {
+        salarioINSS = salarioBruto * 0.92;
+    }else if (salarioBruto > 1556.95 && salarioBruto < 2594.92) {
+        salarioINSS = salarioBruto * 0.91;
+    }else if (salarioBruto > 2594.93 && salarioBruto < 5189.82) {
+        salarioINSS = salarioBruto * 0.89;
+    }else if (salarioBruto > 5189.82) {
+        salarioINSS = salarioBruto - 570.88;
+    }
+}else {
+    console.log('Salario invalido')
+    return;
 }
 
 if (salarioINSS < 1903.98) {
