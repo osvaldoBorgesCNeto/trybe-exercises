c9e8027cd7033374ef4b89321f8f329a65d98608
Osvaldo Borges <netoo70@gmail.com>
2020-11-27 19:39:19 -0300

    Feito ate o exercicio 03

--- exercises/4.3/script.js
+++ exercises/4.3/script.js
@@ -10,3 +10,49 @@ if (somaFrutas > 15) {
 }else {
     console.log('Valor menor que 16');
 }
+
+// Exercicio 01
+console.log('Exercicio 01');
+
+let valorN = 5;
+let quadrado = '';
+
+for (let indexI = 0; indexI < valorN; indexI += 1) {
+    quadrado += '*';
+}
+for (let indexJ = 0; indexJ < valorN; indexJ += 1) {
+    console.log(quadrado); 
+}
+
+
+// Exercicio 02
+console.log('Exercicio 02');
+
+quadrado = '';
+
+for (let indexI = 0; indexI < valorN; indexI += 1) {
+    quadrado += '*';
+    console.log(quadrado);
+}
+
+// Exercicio 03
+console.log('Exercicio 03');
+
+quadrado = '';
+let inputP = valorN;
+
+for (let indexJ = 0; indexJ < valorN; indexJ += 1){
+    for (let indexI = 0; indexI <= valorN; indexI += 1) {
+        if (indexI < inputP) {
+            quadrado += ' ';
+        }else {
+            quadrado += '*';
+        }
+    }
+    console.log(quadrado);
+    quadrado = '';
+    inputP -= 1;
+}
+
+// Exercicio 04
+console.log('Exercicio 04');
