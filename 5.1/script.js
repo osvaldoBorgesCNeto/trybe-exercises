698ec39e86b3594d5d1017ca7274d69ea5a73473
Osvaldo Borges <netoo70@gmail.com>
2020-12-02 18:47:46 -0300

    Feito o Exercicio 03

--- 5.1/script.js
+++ 5.1/script.js
@@ -4,10 +4,18 @@ function changeText() {
     paragraphs[1].innerText = 'Descricao de como voce se ve daqui 2 anos.';
 }
 changeText();
-
 // Exercicio 02
 function changeSquareColorToGreen() {
     let square = document.getElementsByClassName('main-content');
     square[0].style.backgroundColor = 'rgb(76, 164,109)';
 }
 changeSquareColorToGreen()
+
+// Exercicio 03
+function changeSquareColorToWhite() {
+    let square = document.getElementsByClassName('center-content');
+    square[0].style.backgroundColor = 'white';
+}
+changeSquareColorToWhite();
+
+// Exercicio 04
