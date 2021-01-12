53f9f1cf92853893be24f39a2824fc8f597cab43
Osvaldo Borges <netoo70@gmail.com>
2021-01-12 18:14:21 -0300

    Parte 01 - 02/02

--- exercises/7.1/script.js
+++ exercises/7.1/script.js
@@ -1,16 +1,5 @@
-function testingScope(escopo) {
-  let ifScope
-  if (escopo) { 
-    ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
-    console.log(ifScope);
-  } else {
-    let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-    console.log(elseScope);
-  }
-  // Se necessário esta linha pode ser removida.
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
-  console.log(`${ifScope} o que estou fazendo aqui ? :O`); 
-}
+// Seu código aqui.
 
-testingScope(false);
+console.log(`Os numeros ${oddsAndEvens.sort(((a, b) => a - b))} se encontram ordenados de forma crescente!`);
