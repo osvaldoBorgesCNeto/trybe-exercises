00d9c3a17a3645a780b01c6a0255649b6802a3df
Osvaldo Borges <netoo70@gmail.com>
2021-01-18 15:35:29 -0300

    array.sort

---
+++ exercises/array.sort.js
@@ -0,0 +1,23 @@
+// PART 01
+
+const food = ['arroz', 'feijão', 'farofa', 'chocolate', 'doce de leite'];
+food.sort();
+console.log(food);
+
+// PARTE 02
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+numbers.sort();
+console.log(numbers);
+
+// PARTE 03
+
+const points = [40, 100, 1, 5, 25, 10];
+points.sort((a, b) => a - b);
+console.log(points); 
+
+// PARTE 04
+
+const points = [40, 100, 1, 5, 25, 10];
+points.sort((a, b) => b - a);
+console.log(points); 
