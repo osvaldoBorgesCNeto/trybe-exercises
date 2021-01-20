974772e1e8ebc42d40d33d54fbd97b2cf2600808
Osvaldo Borges <netoo70@gmail.com>
2021-01-20 16:31:45 -0300

    exercise 01

---
+++ exercises/8.3/exercise01.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+
+const arrays = [
+    ["1", "2", "3"],
+    [true],
+    [4, 5, 6]
+];
+
+// Referencia: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/concat
+
+function flatten() {
+  // escreva seu código aqui
+  const newMatriz = arrays.reduce((acc, array) => acc.concat(array));
+  return newMatriz;
+}
+
+assert.deepStrictEqual(flatten(), ["1", "2", "3", true, 4, 5, 6]);
