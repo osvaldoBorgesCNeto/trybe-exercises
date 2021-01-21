bd18245a80b3684d77e3cefe96805342eaf9f625
Osvaldo Borges <netoo70@gmail.com>
2021-01-21 16:02:05 -0300

    array.destructuring.js

---
+++ exercises/8.4/array.destructuring.js
@@ -0,0 +1,23 @@
+// PARTE 01
+
+const arrayCountries = ['Brazil', 'Japan', 'China', 'Canada'];
+
+const firstCountry = arrayCountries[0];
+const secondCountry = arrayCountries[1];
+const thirdCountry = arrayCountries[2];
+const fourthCountry = arrayCountries[3];
+
+console.log(firstCountry);
+console.log(secondCountry);
+console.log(thirdCountry);
+console.log(fourthCountry);
+
+// PARTE 02
+
+const arrayCountries = ['Brazil', 'Japan', 'China', 'Canada'];
+const [ firstCountry, secondCountry, thirdCountry, fourthCountry ] = arrayCountries;
+
+console.log(firstCountry);
+console.log(secondCountry);
+console.log(thirdCountry);
+console.log(fourthCountry);
