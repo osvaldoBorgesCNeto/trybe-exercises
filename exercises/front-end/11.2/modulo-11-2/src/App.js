eddbc1407b8e6cc9eb793c6cf8236c4557913f0f
Osvaldo Borges <netoo70@gmail.com>
2021-03-24 14:00:38 -0300

    Tudo de front-end

--- exercises/front-end/11.2/modulo-11-2/src/App.js
+++ exercises/front-end/11.2/modulo-11-2/src/App.js
@@ -1,10 +1,4 @@
 import React from 'react';
-
-import AlbumRender from './components_antes/AlbumRender';
-import UserRender from './components_antes/UserRender';
-import OrderRender from './components_antes/OrderRender';
-import Shopping from './components_antes/ShoppingList';
-
 import PokedexRender from './components_depois/PokedexRender';
 
 
